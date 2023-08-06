5bb5fb0b714b1263e9cdd1c422807e07fa6064d4
onuratakan <atadogan06@gmail.com>
2023-08-06 12:24:11 +0000

    Changed version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 
 setup(name='naruno_scan',
-version='0.1.0',
+version='0.1.1',
 description="""Scan website for Naruno""",
 long_description="".join(open("README.md", encoding="utf-8").readlines()),
 long_description_content_type='text/markdown',
